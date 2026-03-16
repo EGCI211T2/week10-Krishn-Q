db2d4c75e54f6b5c1d8207238089e269e10c0256
Krishn-Q <krishn.poo@student.mahidol.edu>
2026-03-16 08:38:44 +0000

    Time

--- Time.h
+++ Time.h
@@ -8,3 +8,44 @@ struct Time{
 //1.  getTime function
 //2. subtract fuctions
 //3. display functions
+
+
+
+
+void getTime(struct Time &t)
+{
+	cout<<"Input Time (h m s) : ";
+	cin>>t.h>>t.m>>t.s;
+}
+
+struct Time Subtract (struct Time t2, struct Time t1)
+{
+	struct Time t3;//t3=t2-t1
+	t3.h=t2.h-t1.h;
+	t3.m=t2.m-t1.m;
+	t3.s=t2.s-t1.s;
+		if(t3.s<0)
+		{
+			t3.m-=1;
+			t3.s+=60;
+		}
+		if(t3.m<0)
+		{
+			t3.h-=1;
+			t3.m+=60;
+		}
+		if(t3.h<0)
+		{
+			t3.h+=24;
+		}
+
+
+
+	return t3;
+}
+void display(struct Time t3)
+{
+
+	cout<<setw(2)<<t3.h<<setfill(':')<<setw(3)<<t3.m<<setfill(':')<<setw(3)<<t3.s;
+	cout<<"\n";
+}
--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 
 using namespace std;
 
+
 #include "Time.h"
 
 int main(){
     struct Time t1,t2,t3;
-    cout<<"What time was it?";
+    cout<<"What time was it?\n";
+
     getTime(t1);
-    cout<<"What time is it now?";
+    cout<<"What time is it now?\n";
+
     getTime(t2);
-    t3=subtract(t2,t1); //t3=t2-t1
+    t3=Subtract(t2,t1); //t3=t2-t1
     cout<<"Time diff is ";
+
     display(t3);
 }
